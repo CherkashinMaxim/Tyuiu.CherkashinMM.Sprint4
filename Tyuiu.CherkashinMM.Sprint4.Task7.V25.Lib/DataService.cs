db8f455fbc01af4a441b763963324d2581e44122
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-11-13 01:14:16 +0500

    Task7

--- Tyuiu.CherkashinMM.Sprint4.Task7.V25.Lib/DataService.cs
+++ Tyuiu.CherkashinMM.Sprint4.Task7.V25.Lib/DataService.cs
@@ -6,18 +6,13 @@ public class DataService : ISprint4Task7V25
 {
     public int Calculate(int n, int m, string value)
     {
+        int sum = 0;
         int[,] arr = new int[n, m];
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 arr[i, j] = int.Parse(value.Substring(i * m + j, 1));
-
-
-        int sum = 0;
-        for (int i = 0; i < arr.GetLength(0); i++)
-            for (int j = 0; j < arr.GetLength(1); j++)
                 if (arr[i, j] % 2 == 0)
                     sum += arr[i, j];
-
         return sum;
     }
 } 
